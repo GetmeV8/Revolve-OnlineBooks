e648f611ef8c1ae8155bbfb46ad946d44944a165
rameez <rameez.manoli786@gmail.com>
2023-04-27 15:59:35 +0530

    env mongo

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state = {
 }
 
 module.exports.connect = function(done){
-    const url = 'mongodb+srv://rameezmanoli:<NTLlytd4RI6GW6po>@cluster0.f3xcgb1.mongodb.net/test';
+    const url = 'mongodb+srv://rameezmanoli:NTLlytd4RI6GW6po@cluster0.f3xcgb1.mongodb.net/test';
     const dbname = 'Revolve' ;
 
     MongoClient.connect(url,(err,data)=>{
